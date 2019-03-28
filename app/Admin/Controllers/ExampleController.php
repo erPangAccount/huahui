14c185ac366ef53d2a289430e0dd1cf88cdaef96
erPang <a1634621150@gmail.com>
2019-03-28 19:50:44 +0800

    初次使用laravel-admin

--- app/Admin/Controllers/ExampleController.php
+++ app/Admin/Controllers/ExampleController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
 use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
@@ -22,54 +23,54 @@ class ExampleController extends Controller
     public function index(Content $content)
     {
         return $content
-            ->header('Index')
-            ->description('description')
+            ->header('列表')
+            ->description('用户列表')
             ->body($this->grid());
     }
 
     /**
      * Show interface.
      *
-     * @param mixed   $id
+     * @param mixed $id
      * @param Content $content
      * @return Content
      */
     public function show($id, Content $content)
     {
         return $content
-            ->header('Detail')
-            ->description('description')
+            ->header('详情')
+            ->description('用户详情')
             ->body($this->detail($id));
     }
 
-    /**
-     * Edit interface.
-     *
-     * @param mixed   $id
-     * @param Content $content
-     * @return Content
-     */
-    public function edit($id, Content $content)
-    {
-        return $content
-            ->header('Edit')
-            ->description('description')
-            ->body($this->form()->edit($id));
-    }
+//    /**
+//     * Edit interface.
+//     *
+//     * @param mixed   $id
+//     * @param Content $content
+//     * @return Content
+//     */
+//    public function edit($id, Content $content)
+//    {
+//        return $content
+//            ->header('编辑')
+//            ->description('编辑用户')
+//            ->body($this->form()->edit($id));
+//    }
 
-    /**
-     * Create interface.
-     *
-     * @param Content $content
-     * @return Content
-     */
-    public function create(Content $content)
-    {
-        return $content
-            ->header('Create')
-            ->description('description')
-            ->body($this->form());
-    }
+//    /**
+//     * Create interface.
+//     *
+//     * @param Content $content
+//     * @return Content
+//     */
+//    public function create(Content $content)
+//    {
+//        return $content
+//            ->header('新增')
+//            ->description('新增用户')
+//            ->body($this->form());
+//    }
 
     /**
      * Make a grid builder.
@@ -78,45 +79,78 @@ class ExampleController extends Controller
      */
     protected function grid()
     {
-        $grid = new Grid(new YourModel);
+        $grid = new Grid(new Customer());
 
         $grid->id('ID')->sortable();
-        $grid->created_at('Created at');
-        $grid->updated_at('Updated at');
+        $grid->column('email', '邮箱');
+        $grid->column('mobile', '手机号');
+        $grid->column('nick', '昵称');
+        $grid->column('created_at', '创建时间')->sortable();
+        $grid->column('updated_at', '更新时间')->sortable();
 
+        $grid->filter(function ($filter) {
+            // 去掉默认的id过滤器
+            $filter->disableIdFilter();
+
+            // 在这里添加字段过滤器
+            $filter->equal('email', '邮箱');
+            $filter->equal('mobile', '手机号');
+            $filter->like('nick', '昵称');
+        });
+
+
+        $grid->actions(function (Grid\Displayers\Actions $actions) {
+            $actions->disableEdit();
+        });
+        $grid->disableCreateButton();
         return $grid;
     }
 
     /**
      * Make a show builder.
      *
-     * @param mixed   $id
+     * @param mixed $id
      * @return Show
      */
     protected function detail($id)
     {
-        $show = new Show(YourModel::findOrFail($id));
+        $show = new Show(Customer::findOrFail($id));
 
-        $show->id('ID');
-        $show->created_at('Created at');
-        $show->updated_at('Updated at');
+        $show->fields([
+            'id' => 'ID',
+            'email' => '邮箱',
+            'mobile' => '手机号',
+            'nick' => '昵称',
+            'created_at' => '创建时间',
+            'updated_at' => '更新时间'
+        ]);
 
         return $show;
     }
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
-    {
-        $form = new Form(new YourModel);
-
-        $form->display('id', 'ID');
-        $form->display('created_at', 'Created At');
-        $form->display('updated_at', 'Updated At');
-
-        return $form;
-    }
+//    /**
+//     * Make a form builder.
+//     *
+//     * @return Form
+//     */
+//    protected function form()
+//    {
+//        $form = new Form(new Customer());
+//
+//        $form->display('id', 'ID');
+//        $form->text('email', '邮箱');
+//
+//        $form->footer(function ($footer) {
+//            // 去掉`查看`checkbox
+//            $footer->disableViewCheck();
+//
+//            // 去掉`继续编辑`checkbox
+//            $footer->disableEditingCheck();
+//
+//            // 去掉`继续创建`checkbox
+//            $footer->disableCreatingCheck();
+//        });
+//
+//        return $form;
+//    }
 }
